e415c48d49ccc038359027019236991cdd0e27f9
desivar <jillian_desire@yahoo.es>
2025-08-01 22:37:04 -0600

    corect server typo

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors'); // <-- Add this line
 require('dotenv').config();
 
 const app = express();
